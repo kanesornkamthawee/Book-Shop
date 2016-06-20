e57a38fdcb801dffd0cd8a6c8d8bc2d3834b4511
kanesorn <joeey_wk@hotmail.com>
2016-06-20 13:41:03 +0700

    Intent to SignUp

--- app/src/main/java/com/neu/kansorn/bookshop/MainActivity.java
+++ app/src/main/java/com/neu/kansorn/bookshop/MainActivity.java
@@ -1,7 +1,11 @@
 package com.neu.kansorn.bookshop;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+    }// Main Method
+
+    public void clickSignUpMain(View view){
+
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+
+
     }
-}
+
+} // Main Class
